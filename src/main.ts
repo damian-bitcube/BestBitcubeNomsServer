8fd6ec03619dbf2f30bac2bdb952b49b50226d05
dscube <damian@bitcube.tech>
2022-08-19 00:40:06 +0200

    updates

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
+// const express = require('express');
+// const app = express();
 import { createServer } from "http";
 import { createApplication } from "./app";
 import { InMemoryOrderRepository } from "./order-management/order.repository";
+// var cors = require('cors');
+// // app.use(cors());
+// app.use(cors({
+//     'allowedHeaders': ['*'],
+//     // 'exposedHeaders': ['sessionId'],
+//     'origin': '*',
+//     'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     // 'preflightContinue': false
+//   }));
 
+
+// app.use( (req: any, res: any, next: () => void) =>{
+
+//   // Website you wish to allow to connect
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   // res.setHeader('Access-Control-Allow-Methods', '*');
+
+//   // Request headers you wish to allow
+//   res.setHeader('Access-Control-Allow-Headers', '*');
+
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   // res.setHeader('Access-Control-Allow-Credentials', true);
+
+//   // Pass to next layer of middleware
+//   next();
+// });
+
+// const httpServer = createServer(app);
 const httpServer = createServer();
+
 console.log('started')
 createApplication(
   httpServer,
@@ -11,8 +45,8 @@ createApplication(
   },
   {
     cors: {
-      origin: ["http://localhost:4200"],
-    },
+      origin: '*'
+    }
   }
 );
 
